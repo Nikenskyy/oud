c820e91e28dbff5b117c75b020f11ed52617f4b8
Nikenskyy <255663@edu.p.lodz.pl>
2024-12-16 09:00:07 +0100

    ej

--- main.c
+++ main.c
@@ -5,11 +5,15 @@ int policz(int a,int b) {
 int odejmij(int a,int b) {
     return a-b;
 }
+int mnoz(int a,int b) {
+    return a*b;
+}
 int main(void)
 {
     printf("Hello, World!\n");
     printf("peirwszy branch 2+3 = %d", policz(2,3));
     printf("commit2 2-3 = %d", odejmij(2,3));
+    printf("mnoz(2,3) = %d", mnoz(2,3));
     return 0;
 
 }
