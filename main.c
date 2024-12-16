645c7cdfecd9f51e6dd4d28266073596a6589d80
Nikenskyy <255663@edu.p.lodz.pl>
2024-12-16 08:54:40 +0100

    ej

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+int policz(int a,int b) {
+    return a+b;
+}
 int main(void)
 {
     printf("Hello, World!\n");
+    printf("peirwszy branch 2+3 = %d", policz(2,3));
     return 0;
 
 }
